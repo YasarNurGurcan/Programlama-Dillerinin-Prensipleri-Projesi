fd239e394566f8f0b30ce2444f1b6fd7c1700c59
YasarNurGurcan <ynurgurcan979@gmail.com>
2026-05-13 21:01:53 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-String kaynakKod=
+String kaynakKod="hayvanEkle\"Inek\";\n"+
     "yemVer 10;\n"+
 	"sutSag;\n"+
     "hasatYap;";
